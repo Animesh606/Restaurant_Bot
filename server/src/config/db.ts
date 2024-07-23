1f14c7809f4125bd6eff59c66763a06570defd74
Animesh <2025.cse18.animeshbag@gmail.com>
2024-07-23 15:45:32 +0530

    Database connection added

---
+++ server/src/config/db.ts
@@ -0,0 +1,13 @@
+import mysql from "mysql2/promise";
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 10,
+    waitForConnections: true,
+    queueLimit: 0
+});
+
+export default db;
